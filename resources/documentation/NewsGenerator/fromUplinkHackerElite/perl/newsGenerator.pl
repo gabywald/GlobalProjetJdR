7cf31546a0a9850c9cc73dc76c773a4adb205185
Gaby Wald <30340425+gabywald@users.noreply.github.com>
2025-10-24 11:54:48 +0200

    [generator][titres][cyberpunk]Some ameliorations (perl)

--- resources/documentation/NewsGenerator/fromUplinkHackerElite/perl/newsGenerator.pl
+++ resources/documentation/NewsGenerator/fromUplinkHackerElite/perl/newsGenerator.pl
@@ -3,6 +3,8 @@
 use strict;
 use JSON;
 
+use POSIX qw(strftime);
+
 ## TODO get arguments from user
 ## TODO replacing the uppercase elements (if present) : 
 ##    - PERSONNAME (generate one name) ; 
@@ -36,13 +38,13 @@ sub generateNews {
 	my $completeBuildNews = "";
 	
 	## Add / select title
-	$completeBuildNews .= selectELTinNews($newsHash, "titles");
+	$completeBuildNews .= selectELTinNews($newsHash, "titles")."\n";
 	## Add / select content[1-5]
-	$completeBuildNews .= selectELTinNews($newsHash, "content1");
-	$completeBuildNews .= selectELTinNews($newsHash, "content2");
-	$completeBuildNews .= selectELTinNews($newsHash, "content3");
-	$completeBuildNews .= selectELTinNews($newsHash, "content4");
-	$completeBuildNews .= selectELTinNews($newsHash, "content5");
+	$completeBuildNews .= selectELTinNews($newsHash, "content1")."\n";
+	$completeBuildNews .= selectELTinNews($newsHash, "content2")."\n";
+	$completeBuildNews .= selectELTinNews($newsHash, "content3")."\n";
+	$completeBuildNews .= selectELTinNews($newsHash, "content4")."\n";
+	$completeBuildNews .= selectELTinNews($newsHash, "content5")."\n";
 	
 	return $completeBuildNews;
 }
@@ -50,30 +52,52 @@ sub generateNews {
 sub generateNewsToLaTeX {
 	my ($DBnews) = @_;
 	my @newsHashArray = @{$DBnews};
-	my $newsHash = @newsHashArray[int(rand(@newsHashArray))];
+	my $newsHashMAIN = @newsHashArray[int(rand(@newsHashArray))];
+	my $newsHash0001 = @newsHashArray[int(rand(@newsHashArray))];
+	my $newsHash0002 = @newsHashArray[int(rand(@newsHashArray))];
+	my $newsHash0003 = @newsHashArray[int(rand(@newsHashArray))];
+	my $newsHash0004 = @newsHashArray[int(rand(@newsHashArray))];
+	my $newsHash0005 = @newsHashArray[int(rand(@newsHashArray))];
 	
 	my $toReturn = "";
 	
 	$toReturn .= "\\input{../latexTemplates/template_journal_header.tex}\n\n";
-	$toReturn .= "\\large{\\centering \\setmainfont{Sprawl} ".selectELTinNews($newsHash, "titles")."}\n\n";
+	$toReturn .= "\\LARGE{\\centering \\setmainfont{Sprawl} ".selectELTinNews($newsHashMAIN, "titles")."}\n\n";
 	$toReturn .= "\\begin{multicols}{2}\n\n";
 	$toReturn .= "\\small\n\n";
 	$toReturn .= "{\\centering \\Huge \\setmainfont{FoglihtenDeH02} \\textbf{Q+\\_9\\_+q} }~\\\\\n\n";
-	$toReturn .= "\\textbf{".selectELTinNews($newsHash, "head")."}~\\\\\n\n";
-	$toReturn .= "\\emph{".selectELTinNews($newsHash, "content1")."}~\\\\\n\n";
-	$toReturn .= "".selectELTinNews($newsHash, "content2")."\n\n";
-	$toReturn .= "".selectELTinNews($newsHash, "content3")."\n\n";
-	$toReturn .= "".selectELTinNews($newsHash, "content4")."\n\n";
-	$toReturn .= "".selectELTinNews($newsHash, "content5")."\n\n";
+	$toReturn .= "\\textbf{".selectELTinNews($newsHashMAIN, "head")."}~\\\\\n";
+	$toReturn .= "\\emph{".selectELTinNews($newsHashMAIN, "content1")."}~\\\\\n";
+	$toReturn .= "".selectELTinNews($newsHashMAIN, "content2")."\n";
+	$toReturn .= "".selectELTinNews($newsHashMAIN, "content3")."\n";
+	$toReturn .= "".selectELTinNews($newsHashMAIN, "content4")."\n";
+	$toReturn .= "".selectELTinNews($newsHashMAIN, "content5")."~\\\\\n\n";
+	$toReturn .= "\\vfill~\\vfill\n\n";
+	$toReturn .= "\\textbf{".selectELTinNews($newsHash0001, "titles")."}~\\\\\n";
+	$toReturn .= "\\emph{".selectELTinNews($newsHash0001, "head")."}~\\\\\n";
+	$toReturn .= "".selectELTinNews($newsHash0001, "content1")."~\\\\\n";
+	$toReturn .= "".selectELTinNews($newsHash0001, "content2")."\n";
+	$toReturn .= "".selectELTinNews($newsHash0001, "content3")."\n";
+	$toReturn .= "".selectELTinNews($newsHash0001, "content4")."\n";
+	$toReturn .= "".selectELTinNews($newsHash0001, "content5")."~\\\\\n\n";
+	$toReturn .= "\\vfill~\\vfill\n\n";
+	$toReturn .= "\\textbf{".selectELTinNews($newsHash0002, "titles")."}~\\\\\n";
+	$toReturn .= "\\emph{".selectELTinNews($newsHash0002, "head")."}~\\\\\n";
+	$toReturn .= "".selectELTinNews($newsHash0002, "content1")."~\\\\\n";
+	$toReturn .= "".selectELTinNews($newsHash0002, "content2")."\n";
+	$toReturn .= "".selectELTinNews($newsHash0002, "content3")."\n";
+	$toReturn .= "".selectELTinNews($newsHash0002, "content4")."\n";
+	$toReturn .= "".selectELTinNews($newsHash0002, "content5")."~\\\\\n\n";
 	$toReturn .= "\\vfill~\\columnbreak\n\n";
 	$toReturn .= "%% {\\small \\lipsum[1-3] }~\\\\\n\n";
 	$toReturn .= "{\\centering \\Huge \\setmainfont{FoglihtenDeH02} \\textbf{\\{1=:=!\\}} }~\\\\\n\n";
 	$toReturn .= "\\begin{multicols}{2}\n\n";
 	$toReturn .= "\\textbf{Specific Article 1}~\\\\\n\n";
-	$toReturn .= "\\emph{Some Catch about Lorem Ipsum}~\\\\\n\n";
+	$toReturn .= "\\emph{Catchphrase Lorem Ipsum}~\\\\\n\n";
 	$toReturn .= "\\lipsum[5-7]~\\\\\n\n";
 	$toReturn .= "\\end{multicols}\n\n";
-	$toReturn .= "{\\centering \\rule{0.34\textwidth}{1pt} }~\\\\\n\n";
+	$toReturn .= "{\\centering \\rule{0.34\\textwidth}{1pt} }~\\\\\n\n";
+	$toReturn .= "Automatic Generated Page. \\textsc{Not real news !}~\\\\\n\\emph{News Generator} by \\textbf{Gaby Wald}~\\\\\n\n";
 	$toReturn .= "\\end{multicols}\n\n";
 	$toReturn .= "\\input{../latexTemplates/template_journal_footer.tex}\n\n";
 	
@@ -84,7 +108,18 @@ sub selectELTinNews {
 	my ($newsHash, $keyToGet) = @_;
 	return ( (exists $newsHash->{$keyToGet}) ? 
 					@{$newsHash->{$keyToGet}}[int(rand(@{$newsHash->{$keyToGet}}))] : 
-					"" )."\n"; ## "NO '".$keyToGet."' !"
+					"" ); ## "NO '".$keyToGet."' !"
+}
+
+sub generateDateYYYMMDDHHMMSS() {
+	# see https://www.tutorialspoint.com/perl/perl_date_time.htm
+	# my @months = qw( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec );
+	# my @days = qw(Sun Mon Tue Wed Thu Fri Sat Sun);
+	# my ($sec,$min,$hour,$mday,$month,$year,$wday,$yday,$isdst) = localtime();
+	# print "$mday $months[$month] $days[$wday]\n";
+	# return $year.$month.$mday.$hour.$min.$sec;
+	# my $datestring = strftime "%a %b %e %H:%M:%S %Y", localtime;
+	return strftime "%Y%m%d%H%M%S", localtime;
 }
 
 ## Usage example
@@ -92,11 +127,24 @@ my $DBnews = loadNewsDatabase('../databases/newsDataBase-starting.json');
 print generateNews( $DBnews );
 
 my $outputDIR = "generatedOutputs";
-my $fileTEXname = "filetobuild.tex"; ## TODO generate UUID / ID with date !
-open (OUTPUT, ">../".$outputDIR."/".$fileTEXname) or die "file cannot be created";
+## TODO generate UUID / ID with date !
+## TODO use later : my $fileTEXname = "filetobuild".generateDateYYYMMDDHHMMSS();
+my $fileTEXname = "filetobuild";
+open (OUTPUT, ">../".$outputDIR."/".$fileTEXname.".tex") or die "file cannot be created";
 print OUTPUT generateNewsToLaTeX( $DBnews );
 close OUTPUT;
 
+## TODO Makefile && make !
+open (INPUTT, "<../latexTemplates/template_Makefile") or die $!;
+open (OUTPUT, ">../".$outputDIR."/Makefile") or die $!; # "file cannot be created";
+print OUTPUT "LATEXFILE=".$fileTEXname;
+while(<INPUTT>) { print OUTPUT $_; }
+close OUTPUT;
+close INPUTT;
+
+chdir("../generatedOutputs/");
+my $output = `make && make clean`;
+chdir("../perl/");
 
 ## Some tests to show data !
 ## for my $news (@{$DBnews}) {
@@ -124,3 +172,9 @@ close OUTPUT;
 ## 			{ print "\t launcher/'uplinkrating' [".$news->{"launcher"}->{"uplinkrating"}."] \n"; } 
 ## 	}
 ## }
+
+## sub ltrim { my $s = shift; $s =~ s/^\s+//;       return $s };
+## sub rtrim { my $s = shift; $s =~ s/\s+$//;       return $s };
+## sub  trim { my $s = shift; $s =~ s/^\s+|\s+$//g; return $s };
+## use String::Util qw(trim);
+## use Text::Trim qw(trim);
